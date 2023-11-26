24df288118487414d61a34a0e49ce20565680031
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-11-26 17:29:16 +0200

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan((tokens, req, res) => {
   return [
     tokens.method(req, res),
@@ -102,7 +105,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
