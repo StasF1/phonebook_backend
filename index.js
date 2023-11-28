22de53b453a97b2080ac68171580741b7e089086
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-11-28 16:45:42 +0200

    3.14: Phonebook database, step2

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 const cors = require('cors')
 const express = require('express')
 const morgan = require('morgan')
+
+const Person = require('./models/person')
 
 const app = express()
 
@@ -19,34 +23,7 @@ app.use(morgan((tokens, req, res) => {
 }))
 
 let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
 ]
-
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
-}
 
 app.get('/info', (req, res) => {
   res.send(
@@ -56,18 +33,11 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => res.json(persons))
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
+  Person.findById(req.params.id).then(person => res.json(person))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -94,15 +64,11 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
-  const person = {
-    id: generateId(),
+  const person = new Person({
     name: body.name,
     number: body.number,
-  }
-
-  persons = persons.concat(person)
-
-  res.json(person)
+  })
+  person.save().then(savedPerson => response.json(savedPerson))
 })
 
 const PORT = process.env.PORT || 3001
