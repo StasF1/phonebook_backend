9ce94a40ba4524833157fb170c531134395e67ca
Stanislau Stasheuski <stanislau.stasheuski@gmail.com>
2023-11-28 18:16:54 +0200

    uibuild

--- index.js
+++ index.js
@@ -86,8 +86,7 @@ app.post('/api/persons', (request, response, next) => {
     name: name,
     number: number,
   })
-  person
-    .save()
+  person.save()
     .then(savedPerson => response.json(savedPerson))
     .catch(error => next(error))
 })
